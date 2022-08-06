493f97a2c2b9bc2d8529bf72a4259dd150e30f00
wrongerror <wangyifei@kubesphere.io>
2022-08-06 20:26:57 +0800

    add go117 builder & bump ff-go to v0.4.0
    Signed-off-by: wrongerror <wangyifei@kubesphere.io>

---
+++ builders/go117/stack/build.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# The build.sh script builds stack images for the gcp/base builder.
+#
+# The script builds the following two images:
+#   gcr.io/buildpacks/gcp/run:$tag
+#   gcr.io/buildpacks/gcp/build:$tag
+#
+# It also validates that the build image includes all required licenses.
+#
+# Usage:
+#   ./build.sh <path to self> <path to licenses.tar>
+
+set -euo pipefail
+
+# Convenient way to find the runfiles directory containing the Dockerfiles.
+# $0 is in a different directory top-level directory.
+DIR="$(dirname "$1")"
+LICENSES="$2"
+
+TAG="v2.4.0-1.17"
+
+# Extract licenses.tar because it is symlinked, which Docker does not support.
+readonly TEMP="$(mktemp -d)"
+trap "rm -rf $TEMP" EXIT
+
+echo "> Extracting licenses tar"
+mkdir -p "$TEMP/licenses"
+tar xf "$LICENSES" -C "$TEMP/licenses"
+
+echo "> Building base go117common image"
+docker build -t "go117common" - < "${DIR}/parent.Dockerfile"
+echo "> Building base go117run image"
+docker build -t "go117run" - < "${DIR}/run.busybox.Dockerfile"
+echo "> Building openfunctiondev/buildpacks-run-go:$TAG"
+docker build --build-arg "from_image=go117run" -t "openfunctiondev/buildpacks-run-go:$TAG" - < "${DIR}/run.Dockerfile"
+echo "> Building openfunctiondev/buildpacks-go117-build:$TAG"
+cat "${DIR}/build.Dockerfile" | docker build --build-arg "from_image=go117common" -t "openfunctiondev/buildpacks-go117-build:$TAG" -f - "${TEMP}"
---
+++ builders/go117/stack/run.distroless.Dockerfile
@@ -0,0 +1,26 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+FROM openfunctiondev/distroless-static:nonroot 
+
+# The uid and gid for a distroless nonroot user is 65532 
+# according to https://github.com/GoogleContainerTools/distroless/issues/443
+ARG cnb_uid=65532
+ARG cnb_gid=65532
+ARG stack_id="openfunction.go117"
+LABEL io.buildpacks.stack.id=${stack_id}
+
+ENV CNB_USER_ID=${cnb_uid}
+ENV CNB_GROUP_ID=${cnb_gid}
+ENV CNB_STACK_ID=${stack_id}
